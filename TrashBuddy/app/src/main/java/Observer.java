0643bde785a0ce4c281dda801408edcd788533da
Ali <muntasir20@gmail.com>
2017-01-20 21:49:26 -0500

    hard coded Compare

--- TrashBuddy/app/src/main/java/Observer.java
+++ TrashBuddy/app/src/main/java/Observer.java
@@ -1,6 +1,17 @@
 /**
  * Created by Ali on 2017-01-20.
  */
+import
 
 public class Observer {
+    public image compare(image previous, image current){
+        for (int i=0; i < previous.length; i++){
+            for (int j=0; j < previous.width; j++){
+                if (previous.pixelAt(i, j).getRGB() != current.pixelAt(i,j).getRGB){
+                    return current;
+                }
+            }
+        }
+        return null;
+    }
 }
