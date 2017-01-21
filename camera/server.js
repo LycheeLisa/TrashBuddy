637eaa01c2d1da3ea719cf94c6e8178a28e87d87
TheInvoker <ryans.dsouza@mail.utoronto.ca>
2017-01-21 14:27:28 -0500

    cleanup

--- camera/server.js
+++ camera/server.js
@@ -1,28 +1,24 @@
 var express = require('express');
 var app = express();
-var crypto = require('crypto'),
-  fs = require("fs"),
-  https = require("https");
+var fs = require("fs");
+var https = require("https");
 
-  
-  
+
 var privateKey  = fs.readFileSync('sslcert/key.pem', 'utf8');
 var certificate = fs.readFileSync('sslcert/cert.pem', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 
 app.use(express.static(__dirname + '/public'));
 
+
+
 /*
  * Visit the home page
  */
 app.get('/', function (req, res) {
-	//console.log(1);
 	res.header('Access-Control-Allow-Origin', '*');
 	res.sendFile(__dirname + '/public/camera.html');
-	//res.send("done");
 });
-
-
 
 var httpsServer = https.createServer(credentials, app);
 httpsServer.listen(process.env.PORT || 3000, function () {
