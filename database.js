6425187221c190ad69bf2c0d7913c56c9a960747
Lisa Chen <lisajia.chen@mail.utoronto.ca>
2017-01-21 13:45:50 -0500

    added database and server

--- database.js
+++ database.js
@@ -1,62 +1,71 @@
 /**
  * Created by chenji13 on 1/21/2017.
  */
-
+console.log("hi");
 window.smartcan_db = {
     "data" : [
         {
             'name':'banana',
             'price':0.50,
             'category':'fruit',
-            'material' :'organic'
+            'material' :'organic',
+            'time': 0
         },
         {
             'name':'water bottle',
             'price':2.99,
             'category':'recyclable',
-            'material' :'recyclable'
+            'material' :'recyclable',
+            'time': 0
         },
         {
             'name':'pop can',
             'price':1.99,
             'category':'recyclable',
-            'material' :'recyclable'
+            'material' :'recyclable',
+            'time': 2
         },
         {
             'name':'broccoli',
             'price':4.00,
             'category':'vegetable',
-            'material' :'organic'
+            'material' :'organic',
+            'time': 1
         },
         {
             'name':'egg',
             'price':1.99,
             'category':'protein',
-            'material' :'organic'
+            'material' :'organic',
+            'time': 1
         },
         {
             'name':'chicken',
             'price':7.99,
             'category':'protein',
-            'material' :'organic'
+            'material' :'organic',
+            'time': 2
         },
         {
             'name':'candy wrapper',
             'price':1.99,
             'category':'garbage',
-            'material' :'garbage'
+            'material' :'garbage',
+            'time': 0
         },
         {
             'name':'milk',
             'price':7.99,
             'category':'dairy',
-            'material' :'organic'
+            'material' :'organic',
+            'time': 1
         },
         {
             'name':'salmon',
             'price':8.99,
             'category':'protein',
-            'material' :'organic'
+            'material' :'organic',
+            'time': 2
         },
         {
             'name':'cheese',
@@ -68,61 +77,71 @@ window.smartcan_db = {
             'name':'cauliflower',
             'price':8.99,
             'category':'vegetable',
-            'material' :'organic'
+            'material' :'organic',
+            'time': 2
         },
         {
             'name':'bread',
             'price':4.99,
             'category':'grains',
-            'material' :'organic'
+            'material' :'organic',
+            'time': 0
         },
         {
             'name':'cake',
             'price':12.99,
             'category':'other',
-            'material' :'organic'
+            'material' :'organic',
+            'time': 1
         },
         {
             'name':'paper',
             'price':0.50,
             'category':'recyclable',
-            'material' :'recyclable'
+            'material' :'recyclable',
+            'time': 2
         },
         {
             'name':'takeout',
             'price':0.50,
             'category':'garbage',
-            'material' :'garbage'
+            'material' :'garbage',
+            'time': 0
         },
         {
             'name':'apple',
             'price':1.99,
             'category':'fruit',
-            'material' :'organic'
+            'material' :'organic',
+            'time': 1
         },
         {
             'name':'orange',
             'price':1.99,
             'category':'fruit',
-            'material' :'organic'
+            'material' :'organic',
+            'time': 1
         },
         {
             'name':'cherry',
             'price':7.99,
             'category':'fruit',
-            'material' :'organic'
+            'material' :'organic',
+            'time': 2
         },
         {
             'name':'pear',
             'price':2.99,
             'category':'fruit',
-            'material' :'organic'
+            'material' :'organic',
+            'time': 0
         },
         {
             'name':'yogurt',
             'price':9.99,
             'category':'recyclable',
-            'material' :'recyclable'
+            'material' :'recyclable',
+            'time': 1
         }
     ]
 };
--- server.js
+++ server.js
@@ -3,11 +3,36 @@
  */
 var express = require('express');
 var app = express();
+var db = require('./db.json');
 app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res ) {
     res.sendFile(__dirname + '/public/index.html');
+    // res.sendFile(__dirname + '/public/js/custom.js');
 });
-
+app.get('/api/daily', function(req, res){
+    var newDatabase = {
+        "data": db.data.filter(function(item){
+            return item.time == 0;
+        })
+    };
+    res.send(JSON.stringify(newDatabase));
+});
+app.get('/api/weekly', function(req, res){
+    var newDatabase = {
+      "data": db.data.filter(function(item){
+          return item.time <= 1;
+      })
+    };
+    res.send(JSON.stringify(newDatabase));
+});
+app.get('/api/monthly', function(req, res){
+    var newDatabse = {
+        "data": db.data.filter(function(item){
+            return item.time <= 2;
+        })
+    };
+    res.send(JSON.stringify(newDatabse));
+});
 var server = app.listen(process.env.PORT || 3000, function () {
     var host = server.address().address;
     var port = server.address().port;
